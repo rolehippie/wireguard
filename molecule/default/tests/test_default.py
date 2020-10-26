d77bace6c8f08194f13aaebea759ca1f8ba8d42a
Thomas Boerger <thomas@webhippie.de>
2020-10-26 14:30:32 +0100

    Change test to any ip, not only localhost

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -18,4 +18,4 @@ def test_running_and_enabled(host):
 
 
 def test_server_socket(host):
-    assert host.socket("tcp://127.0.0.1:51820").is_listening
+    assert host.socket("tcp://0.0.0.0:51820").is_listening
